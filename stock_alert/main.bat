c6956783662c7cea6b9b62e803de2d6408baf5ef
ChaneWq <854151718@qq.com>
2025-11-10 16:36:08 +0800

    1

--- stock_alert/main.bat
+++ stock_alert/main.bat
@@ -3,5 +3,5 @@ chcp 65001 > nul
 echo 正在运行Python脚本...
 d:
 cd D:\wu\pystock\stock_alert
-python D:\wu\pystock\stock_alert\main.py
+python main.py
 pause
--- stock_alert/scheduler.py
+++ stock_alert/scheduler.py
@@ -1,4 +1,8 @@
 # scheduler.py
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import time
 import threading
 import task_manager
