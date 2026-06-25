0f1568a5acf1e5588edb33428b3e6c16864d4c3f
ChaneWq <854151718@qq.com>
2026-06-25 20:16:32 +0800

    1

--- demo/stock_minute_data/fetch_minute_data.py
+++
@@ -1,6 +0,0 @@
-from day_index import init_create_client
-client = init_create_client()
-code = '000400'
-df = client.bars(symbol=code, frequency=7, offset=1600)
-df.to_csv('000400.csv', index=False)
-
